f09ec71a6ca0fba737a809b2b0bc2670d15bf451
janmejay25 <janmjaypandya@gmail.com>
2025-01-08 09:55:22 +0530

    calculator added

---
+++ calculator.py
@@ -0,0 +1,3 @@
+num1 = int((input("enter number")))
+num2 = int((input("enter number")))
+print("addition is" , num1 +num2 )
--- hello.py
+++ hello.py
@@ -1,4 +1,1 @@
 print("hello world")
-num1 = int((input("enter number")))
-num2 = int((input("enter number")))
-print("addition is" , num1 +num2 )
