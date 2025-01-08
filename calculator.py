dc1f4cae3b91335e35134b1d52d67b2a1d533361
janmejay25 <janmjaypandya@gmail.com>
2025-01-08 09:59:40 +0530

    all operation added

--- calculator.py
+++ calculator.py
@@ -1,3 +1,13 @@
-num1 = int((input("enter number")))
-num2 = int((input("enter number")))
-print("addition is" , num1 +num2 )
+num1 = int((input("enter number ")))
+num2 = int((input("enter number ")))
+operation = input("enter operation ")
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("invalid operation")
