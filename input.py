d4d1ba36fca58dfbc61a9236ee1141ea487d9d6e
janmejay25 <janmjaypandya@gmail.com>
2025-01-14 09:38:41 +0530

    age input added

---
+++ input.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+age = int(input("enter your age"))
+if age >18 :
+    print("you can vote ")
+else :
+    print("you can`t vote")
+yr_age = 100+(2025 -age)
+print("your birth year is",2025-age)
+print("100 yr completed in ",yr_age)
+
