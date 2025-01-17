8094e998d2079cf25bafd20086d7c7553520c086
janmejay25 <janmjaypandya@gmail.com>
2025-01-17 19:18:13 +0530

    for loop types

--- loop.py
+++ loop.py
@@ -1,2 +1,13 @@
 for i in range(10):
     print(i)
+
+string = "hello"
+for char in string:
+    print(char)
+
+for i in range (0,6):
+    print(i)
+
+table = int(input("enter the table "))
+for i in range(1,11):
+    print(table,"*",i,"=",i*table)
