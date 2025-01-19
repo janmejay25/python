07f6701802cdbef0a15b29b05b3d9920b8f90faf
janmejay25 <janmjaypandya@gmail.com>
2025-01-19 09:53:31 +0530

    function types

---
+++ function.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    return a+b
+def subtract():
+    a = int(input("enter value of A "))
+    b = int(input("enter value of B "))
+    return a - b
+
+a = int(input("enter value of A "))
+b = int(input("enter value of B "))
+
+print ("addition of a & B is ", add(a,b))
+print("subtraction of a & B is ", subtract())
+
+
+def dchap():
+    print("dchap")
+dchap()
+dchap()
+dchap()
