9ab0b1eecc8b0958029dea79ec41b455860030e0
janmejay25 <janmjaypandya@gmail.com>
2025-02-06 17:21:13 +0530

    break continue used

--- whileloop.py
+++ whileloop.py
@@ -13,3 +13,19 @@ while i<=10:
     i=i+1
 print("sum = ",sum)
 # output: sum = 55
+
+i=1
+while i <= 5:
+    if i == 3:
+        break
+    print(i)
+    i = i + 1
+print("End of loop")
+
+i=0
+while i <= 5:
+    i = i + 1
+    if i == 3:
+        continue
+    print(i)
+
