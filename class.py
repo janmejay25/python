a9214a1a3fd7082f4ede37ad03f195bc68b3d9f6
janmejay25 <janmjaypandya@gmail.com>
2025-02-12 19:44:33 +0530

    init used

--- class.py
+++ class.py
@@ -6,3 +6,19 @@ print(myClass.name)
 print(myClass.age)
 
 
+# init used in class
+class car:
+    def __init__(self,name,modal):
+        self.name = name
+        self.modal = modal
+
+    def display(self):
+        print("Name: ",self.name)
+        print("Modal: ",self.modal)
+        return "using retrun type  Name: "+self.name+" Modal: "+str(self.modal)
+
+c1 = car("tiago",2025)
+c2 = car("BMW",2024)
+car.display(c1)
+print(c2.display())
+print(c1.name)
