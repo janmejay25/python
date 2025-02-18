c5274abb5a9a08ea1288c6d373a85bb9fd228cc6
janmejay25 <janmjaypandya@gmail.com>
2025-02-18 19:32:02 +0530

    method OverRiding

---
+++ OOP/method_OverRide.py
@@ -0,0 +1,23 @@
+class human:
+    def speak(self):
+        print("i can speak")
+    def talk(self):
+        print("i can talk")
+
+class male(human):
+    def dance(self):
+        print("i can dance")
+    def talk(self):
+        print("i can talk as class male")
+
+#object for class male
+male_1 = male()
+
+# male function speak and talk
+# inherited from class human
+male_1.speak()
+# talk method is used in class human and male
+# that is called method overloading
+# but it`ll give the output using class male
+# (class male >> class human) beacuse it has method in OG class
+male_1.talk()
