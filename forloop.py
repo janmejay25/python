e0d43491c0bb4fe1618af351f1140cce6dbe1379
janmejay25 <janmjaypandya@gmail.com>
2025-03-18 21:37:43 +0530

    pattern removed

--- forloop.py
+++ forloop.py
@@ -163,32 +163,3 @@ else:
     print("Done!")
 # output: 0 1 2 3 4 5 6 7 8 9 Done!
 
-# for loop z
-for row in range(5):
-    line = ""
-    for col in range(5):
-        if row == 0 or row+col==4 or row==4:
-            line += "*"
-        else:
-            line += " "
-    print(line)
-
-# for lop a
-for row in range(5):
-    line = ""
-    for col in range(5):
-        if (row == 0) or (col==0)or( col==4)or(row==2):
-            line += "*"
-        else:
-            line += " "
-    print(line)
-
-
-# for loop print G
-for row in range(5):
-    line = ""
-    for col in range(5):
-        if (row == 0  and col<4) or (col == 4 and row>1) or (col==2 and row>1) or (row==4 and col<3)or(row>0 and col==0):
-            line += "*"
-        else:
-            line += " "
