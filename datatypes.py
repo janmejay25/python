a0b0e4e6b37ad7d526f3e9f9ace40ce7a8c77256
janmejay25 <janmjaypandya@gmail.com>
2025-04-30 09:14:43 +0530

    logical oper

--- datatypes.py
+++ datatypes.py
@@ -3,3 +3,14 @@ tup = (2,"banana",5,9,8.5)
 print(tup.index("banana")) # 1
 # count the elemt in the tuple
 print(tup.count(2)) # 1 
+
+
+
+# logical operators
+print("logical operators")
+a =10 
+b =4
+print (a and b) # 4
+print (a or b) # 10
+print (not b) # False
+
