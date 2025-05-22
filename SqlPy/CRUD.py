62fdfbc87a98bb3a8d70df92f0e6af7751f2439f
janmejay25 <janmjaypandya@gmail.com>
2025-05-22 22:15:10 +0530

    practice

--- SqlPy/CRUD.py
+++ SqlPy/CRUD.py
@@ -40,7 +40,6 @@ print("New student inserted successfully!")
 # 3️⃣ READ DATA (Retrieve)
 cursor.execute("SELECT * FROM student")
 print("\nStudent Records:")
-
 for row in cursor.fetchall():
     print(row)
 
